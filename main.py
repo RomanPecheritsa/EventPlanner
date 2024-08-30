a657d6a323c90916b347beee626b288790ffeb8e
pecheritsa <pecheritsa.roman@gmail.com>
2024-08-30 23:11:31 +0300

    add SQLLite db, and CRUD operations

--- main.py
+++ main.py
@@ -1,14 +1,20 @@
+from contextlib import asynccontextmanager
 from fastapi import FastAPI
 from fastapi.responses import RedirectResponse
+from database.connection import conn
 
 from routes.users import user_router
 from routes.events import event_router
 
 import uvicorn
 
-app = FastAPI()
 
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    conn()
+    yield
 
+app = FastAPI(lifespan=lifespan)
 app.include_router(user_router,  prefix="/user")
 app.include_router(event_router, prefix="/event")
 
